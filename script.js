00a0c936483f10cb3c5f12dad0d007ede8e7d16b
kellerl3641 <kellerl@nycstudents.net>
2023-12-11 09:15:31 -0500

    save

--- script.js
+++ script.js
@@ -118,30 +118,6 @@ const initTimer = maxTime => {
     }, 1000);
 }
 
-const initGame = () => {
-    initTimer(30);
-    let randomObj = words[Math.floor(Math.random() * words.length)];
-    let wordArray = randomObj.word.split("");
-    for (let i = wordArray.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
-    }
-    wordText.innerText = wordArray.join("");
-    hintText.innerText = randomObj.hint;
-    correctWord = randomObj.word.toLowerCase();;
-    inputField.value = "";
-    inputField.setAttribute("maxlength", correctWord.length);
-}
-initGame();
-
-const checkWord = () => {
-    let userWord = inputField.value.toLowerCase();
-    if(!userWord) return alert("Please enter the word to check!");
-    if(userWord !== correctWord) return alert(`Oops! ${userWord} is not a correct word`);
-    alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
-    initGame();
-}
-
 refreshBtn.addEventListener("click", initGame);
 checkBtn.addEventListener("click", checkWord);
 
